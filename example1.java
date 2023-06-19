025597516f624fa154afab966753031e1077ab0f
Peru <dhanushay2102@gmail.com>
2023-06-19 12:24:44 +0530

    Commiting code and example

---
+++ example1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class example1{
+public static void main(String args[]){
+System.out.println("This is an example of git java file.");
+}
+}
