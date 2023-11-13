17020b9611d02b4c10d24ab7306202402f83a99b
PeruP <137041051+PeruP@users.noreply.github.com>
2023-11-13 19:31:36 +0530

    Delete ex1.java

--- ex1.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-class ex1{
-public static void main(String args[]){
-System.out.println("This is another example of git java file.");
-}
-}
