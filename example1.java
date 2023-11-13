918b8058799e5457b29e795e0bbdc5903a810927
PeruP <137041051+PeruP@users.noreply.github.com>
2023-11-13 19:32:06 +0530

    Delete example1.java

--- example1.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-class example1{
-public static void main(String args[]){
-System.out.println("This is an example of git java file.");
-}
-}
